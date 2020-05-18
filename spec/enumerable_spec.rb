ec73b33fdfaa538d61738823e93770852d31a6a1
Safa ERDEN <safaerden@gmail.com>
2020-05-18 23:44:43 +0300

    fix linter errors

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # spec/enumerables_spec.rb
 
 require_relative '../my_methods.rb'
@@ -11,10 +9,10 @@ RSpec.describe Enumerable do
   let(:test4) { [1, 2, 3, 1, 2, 3, 4, 1, 2, 3] }
 
   describe '#my_each' do
-    it "should interate through an array" do
-      expect(test1.my_each { |x| x }).to eql(test1.each {|x| x })
-      expect(test1.my_each { |x| x * 2 }).to eql(test1.each { |x| x * 2 })
-      expect(test1.my_each { |x| x * 4 }).to eql(test1.each { |x| x * 4 })
+    it 'should interate through an array' do
+      expect(test1.my_each { |x| p x }).to eql(test1.each { |x| p x })
+      expect(test1.my_each { |x| p x * 2 }).to eql(test1.each { |x| p x * 2 })
+      expect(test1.my_each { |x| p x * 4 }).to eql(test1.each { |x| p x * 4 })
     end
   end
 
@@ -59,17 +57,13 @@ RSpec.describe Enumerable do
     end
   end
 
-  describe "#my_map" do
-      it "should apply the logic of the block given to every element of the array" do
-        expect( test1.my_map { |el| el**2 } ).to eq([25, 16, 4])
-      end
+  describe '#my_map' do
+    it 'should apply the logic of the block given to every element of the array' do
+      expect(test1.my_map { |el| el**2 }).to eq([25, 16, 4])
     end
+  end
 
   describe '#my_inject' do
-    it 'should ' do
-      expect(test1.my_inject(5.0, :/) { |sum, n| sum * n }).to eql(test1.inject(5.0, :/) { |sum, n| sum * n })
-    end
-    
     let(:array) { Array.new(10) { rand(0...10) } }
     let(:operation) { proc { |sum, n| sum + n } }
 
@@ -88,5 +82,4 @@ RSpec.describe Enumerable do
       end
     end
   end
-  
 end
