c32c4c6fb1591e4f5285380a6432191d2260cc96
Safa ERDEN <safaerden@gmail.com>
2020-05-25 11:35:22 +0300

    fix linter errors

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -8,13 +8,13 @@ RSpec.describe Enumerable do
   let(:test3) { [nil, true, 10] }
   let(:test4) { [1, 2, 3, 1, 2, 3, 4, 1, 2, 3] }
   let(:test5) { [false, false, false] }
-  let(:test6) { {a:1, b:2, c:3, d:4, e:5} }
+  let(:test6) { { a: 1, b: 2, c: 3, d: 4, e: 5 } }
   let(:range) { (0..10) }
 
   describe '#my_each' do
     it 'should iterate through an array' do
       expect(test1.my_each { |x| p x + 1 }).to eql(test1.each { |x| p x + 1 })
-      expect(test6.my_each { |k,v| p v + 1 }).to eql(test6.each { |k,v| p v + 1 })
+      expect(test6.my_each { |_k, v| p v + 1 }).to eql(test6.each { |_k, v| p v + 1 })
       expect(range.my_each { |x| p x + 1 }).to eql(range.each { |x| p x + 1 })
     end
   end
@@ -29,7 +29,7 @@ RSpec.describe Enumerable do
   describe '#my_select' do
     it 'should create a new array based on the conditions passed at the block' do
       expect(test4.my_select { |x| x > 3 }).to eql([4])
-      expect(test6.my_select { |k,v| v==5 }).to eql(test6.select { |k,v| v==5 })
+      expect(test6.my_select { |_k, v| v == 5 }).to eql(test6.select { |_k, v| v == 5 })
       expect(range.my_select { |x| x > 10 }).to eql(nil)
     end
   end
@@ -37,12 +37,12 @@ RSpec.describe Enumerable do
   describe '#my_all?' do
     it 'should return true if all of the elements of the Enumerable pass the condition given by the block' do
       expect(test1.my_all? { |x| x == 1 }).to eql(test1.all? { |x| x == 1 })
-      expect(test6.my_all? { |k,v| v==5 }).to eql(test6.all? { |k,v| v==5 })
+      expect(test6.my_all? { |_k, v| v == 5 }).to eql(test6.all? { |_k, v| v == 5 })
       expect(range.my_all? { |x| x == 1 }).to eql(range.all? { |x| x == 1 })
     end
     it 'should not return true if any of the elements of the Enumerable pass the condition given by the block' do
       expect(test1.my_all? { |x| x < 1 }).not_to eql(true)
-      expect(test6.my_all? { |k,v| v==5 } ).to eql(false)
+      expect(test6.my_all? { |_k, v| v == 5 }).to eql(false)
       expect(range.my_all? { |x| x < 1 }).not_to eql(true)
     end
   end
@@ -50,12 +50,12 @@ RSpec.describe Enumerable do
   describe '#my_any?' do
     it 'should check whether the given condition is true for every item in the array' do
       expect(test3.my_any? { |x| x == true }).to eql(true)
-      expect(test6.my_any? { |k,v| v==5 } ).to eql(true)
+      expect(test6.my_any? { |_k, v| v == 5 }).to eql(true)
       expect(range.my_any? { |x| x == 3 }).to eql(true)
     end
     it "shouldn't return true if none of the elements of the Enumerable pass the condition given by the block" do
       expect(test5.my_any? { |x| x == true }).not_to eql(true)
-      expect(test6.my_any? { |k,v| v==7 } ).to eql(false)
+      expect(test6.my_any? { |_k, v| v == 7 }).to eql(false)
       expect(range.my_any? { |x| x == 11 }).to eql(false)
     end
   end
@@ -63,12 +63,12 @@ RSpec.describe Enumerable do
   describe '#my_none?' do
     it 'should check if the given condition is true for every item in the array' do
       expect(test4.my_none? { |x| x > 0 }).to eql(false)
-      expect(test6.my_none? { |k,v| v==7 } ).to eql(true)
+      expect(test6.my_none? { |_k, v| v == 7 }).to eql(true)
       expect(range.my_none? { |x| x > 0 }).to eql(false)
     end
     it 'should not return true check if the given condition is false for every item in the array' do
       expect(test4.my_none? { |x| x > 0 }).not_to eql(true)
-      expect(test6.my_none? { |k,v| v==5 } ).to eql(false)
+      expect(test6.my_none? { |_k, v| v == 5 }).to eql(false)
       expect(range.my_none? { |x| x > 0 }).not_to eql(true)
     end
   end
@@ -76,12 +76,12 @@ RSpec.describe Enumerable do
   describe '#my_map' do
     it 'should apply the logic of the block given to every element of the array' do
       expect(test1.my_map { |el| el**2 }).to eq([25, 16, 4])
-      expect(test6.my_map { |k,v| v*0 } ).to eql([0, 0, 0, 0, 0])
-      expect(range.my_map { |el| el**2 }).to eq(range.map { |el| el**2  })
+      expect(test6.my_map { |_k, v| v * 0 }).to eql([0, 0, 0, 0, 0])
+      expect(range.my_map { |el| el**2 }).to eq(range.map { |el| el**2 })
     end
     it 'should\'t apply different logic of the block given to every element of the array' do
       expect(test1.my_map { |el| el**2 }).not_to eq([21, 6, 4])
-      expect(test6.my_map { |k,v| v*0 } ).not_to eql({a:0, b:0, c:0, d:0, e:0})
+      expect(test6.my_map { |_k, v| v * 0 }).not_to eql({ a: 0, b: 0, c: 0, d: 0, e: 0 })
       expect(range.my_map { |el| el**2 }).not_to eq([25, 16, 4])
     end
   end
